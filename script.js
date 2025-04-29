93b634f49bcc1020ea383f929eedf226a9f61ead
miuidroidzz <miuidroid9@gmail.com>
2025-04-29 19:17:16 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('mousemove', (e) => {
-  const bg = document.querySelector('.bg-animation');
-  const x = (e.clientX / window.innerWidth - 0.5) * 30;
-  const y = (e.clientY / window.innerHeight - 0.5) * 30;
-  bg.style.transform = `translate(${x}px, ${y}px)`;
-});
